174e9288a9c662b5d4844617d6ebba0c54609431
Einar Martin Egeland Johnsen <eimj@equinor.com>
2020-07-02 10:08:02 +0200

    Working graphics

--- test_distance.py
+++ test_distance.py
@@ -1,161 +1,96 @@
 from tkinter import *
-import random
-
-
 from board_gen import main as bg
 import numpy as np
-from sklearn.utils.graph import single_source_shortest_path_length
 import time
 
 window = Tk()
 
-board, start, end = bg((15, 15))
+ROW, COL = 15, 15
+
+board, start, end = bg((ROW, COL))
 
 
 np_arr = [0 if x == "*" else 2 if x == "X" else 1 for x in board.values()]
 
 
-arr = np.array(np_arr).reshape((15,15))
-
-#arr[start][end] = 1
-
+arr = np.array(np_arr).reshape((15, 15))
 arr_old = arr.copy()
 arr_old[start][end] = 5
 
-results = {}
+results = []
+
 
 # Inspiration from: https://www.101computing.net/backtracking-maze-path-finder/
-
 def backtracker(board, i, j, results):
-    
-    print(board, i, j, results)
-    
-    print(board[i][j])
-    
     if board[i][j] == 2:
-        results[(i,j)] = 2
-        print("we done")
-        print(board)
+        results.append([(i, j), 2])
         return True
-    
+
     elif board[i][j] == 1:
-        
+
         board[i][j] = 3
-        results[(i,j)] = 3
+        results.append([(i, j), 3])
 
-        if i < len(board)-1 and backtracker(board, i+1, j, results):
+        if i < len(board) - 1 and backtracker(board, i + 1, j, results):
             return True
-        if i > 0 and backtracker(board, i-1, j, results):
+        if i > 0 and backtracker(board, i - 1, j, results):
             return True
-        
-        if j < len(board[0]) and backtracker(board, i, j+1, results):
+
+        if j < len(board[0]) and backtracker(board, i, j + 1, results):
             return True
-        
-        if j > 0 and backtracker(board, i, j-1, results):
+
+        if j > 0 and backtracker(board, i, j - 1, results):
             return True
-        
-        results[(i,j)] = 4
-        board[i][j] = 4 
-        print("pop - backtrack")
+
+        results.append([(i, j), 4])
+        board[i][j] = 4
 
 
-#print(start, end)
-backtracker(arr, start, end, results)
-print(start, end)
-print(results)
-
-#colours = ['red','green','orange','white','yellow','blue']
-#my_dict = {f"{x}_{y}":x*y for x in range(15) for y in range(15)}
-
-my_dict= {}
+my_dict = {}
 
 
-def get_color(value):
-    if value == 0:
+def get_color(loca_value):
+    if loca_value == 0:
         return "black"
-    if value == 1:
+    if loca_value == 1:
         return "red"
-    if value == 2:
+    if loca_value == 2:
         return "green"
-    if value == 5:
+    if loca_value == 5:
         return "blue"
-    if value == 3:
-        return "yellow"
-    if value == 4:
+    if loca_value == 3:
+        return "purple"
+    if loca_value == 4:
         return "gray"
 
-keys = []
-for x in range(15):
-    for y in range(15):
 
-        value = arr_old[x][y]
+def create_labels():
+    for x in range(ROW):
+        for y in range(COL):
 
-        a_key = StringVar()
-        a_key.set(str(value))
+            value = arr_old[x][y]
 
-        my_label = Label(window, textvariable=a_key, bg=get_color(value), relief=RIDGE, width=5)
-        my_label.grid(row=x, column=y)
+            # If using textvariable as the label text
+            # a_key = StringVar()
+            # a_key.set(str(value))
 
-        my_dict[(x,y)] = my_label
+            my_label = Label(
+                window, textvariable="", bg=get_color(value), relief=RIDGE, width=5
+            )
+            my_label.grid(row=x, column=y)
 
+            my_dict[(x, y)] = my_label
 
-#print(my_dict)
-
-
-def test(arr, start, end):
-    print("--")
-    print(arr, start, end)
-    print("test")
 
 def after_runner():
 
-    
-    #if flag:
-    
     for x in results:
-        my_dict[x].config(bg=get_color(results[x]))
+        my_dict[x[0]].config(bg=get_color(x[1]))
         window.update_idletasks()
         time.sleep(0.1)
-    
-    #time.sleep(1)
-    #print(arr)
-    #print(start)
-    #print(end)
 
 
-
-    #print("done")
-
-    
-    #window.update_idletasks()
-
-
-
-    #tk.Label(textvar=str(random.randint(1, 100)),bg="red", width=5).grid(row=10, column=10)
-
-    #for x in range(10):
-    #    for y in range(10):
-    #        
-    #        #time.sleep(1)
-
-
-
-#window.after(5000, after_runner())
-#after_runner()
+backtracker(arr, start, end, results)
+create_labels()
 window.after(2000, after_runner)
 window.mainloop()
-#after_runner()
-
-
-
-
-
-
-
-
-#r = 0
-#for c in colours:
-#    tk.Label(text=c, bg=c, relief=tk.RIDGE, width=10).grid(row=r,column=0)
-#    r = r + 1
-
-#l1 = tk.Label(text="Test", fg="black", bg="white")
--- utils.py
+++ utils.py
@@ -1,24 +1,27 @@
 import subprocess
 import math
 
+
 # Helper functions
 def is_finished(the_map, input_x, input_y):
     if the_map[(input_x, input_y)] == "X":
         print("You are finished, congratz - The reward was a joke. LOLz")
         exit()
-    
+
+
 def allowed_step(the_map, input_x, input_y):
     # Check if it is a valid input
     if the_map[(input_x, input_y)] != "*":
         return True
     return False
 
+
 def traverse_tree(direction, the_map, current_x, current_y, new_pos_x, new_pos_y):
-    
+
     subprocess.run("clear")
     # Check if we are done
     is_finished(the_map, new_pos_x, new_pos_y)
-    
+
     print(f"You chose: {direction}")
 
     # Check if it is an allowed step
@@ -26,7 +29,7 @@ def traverse_tree(direction, the_map, current_x, current_y, new_pos_x, new_pos_y
         print("That was a valid move")
         current_x, current_y = new_pos_x, new_pos_y
         return current_x, current_y
-    
+
     # If it was not a valid move or we did not find the end, we let the user try again
     print("Invalid move.. Please try again")
     return current_x, current_y
@@ -35,37 +38,40 @@ def traverse_tree(direction, the_map, current_x, current_y, new_pos_x, new_pos_y
 def print_state():
     temp_list = list(local_dict.values())
     list_len = int(math.sqrt(len(temp_list)))
-    chunks = [temp_list[x:x+list_len] for x in range(0, len(temp_list), list_len)]
+    chunks = [temp_list[x : x + list_len] for x in range(0, len(temp_list), list_len)]
     for chunk in chunks:
-        print("".join(chunk)+"\n")
+        print("".join(chunk) + "\n")
 
-    
+
 def set_current_state(POS_X, POS_Y):
     # Visited Nodes
     local_dict[(POS_X, POS_Y)] = "*"
 
+
 def get_state(POS_X, POS_Y):
     local_dict[(POS_X, POS_Y)] = "X"
 
+
 def handle_move(the_map, direction, POS_X, POS_Y):
-    
+
     set_current_state(POS_X, POS_Y)
 
     if direction == "up":
-       POS_X, POS_Y = traverse_tree(direction, the_map, POS_X,POS_Y,POS_X-1,POS_Y)
+        POS_X, POS_Y = traverse_tree(direction, the_map, POS_X, POS_Y, POS_X - 1, POS_Y)
     elif direction == "down":
-       POS_X, POS_Y = traverse_tree(direction, the_map, POS_X,POS_Y,POS_X+1,POS_Y)
+        POS_X, POS_Y = traverse_tree(direction, the_map, POS_X, POS_Y, POS_X + 1, POS_Y)
     elif direction == "left":
-       POS_X, POS_Y = traverse_tree(direction, the_map, POS_X,POS_Y,POS_X,POS_Y-1)
+        POS_X, POS_Y = traverse_tree(direction, the_map, POS_X, POS_Y, POS_X, POS_Y - 1)
     elif direction == "right":
-       POS_X, POS_Y = traverse_tree(direction, the_map, POS_X,POS_Y,POS_X,POS_Y+1)
+        POS_X, POS_Y = traverse_tree(direction, the_map, POS_X, POS_Y, POS_X, POS_Y + 1)
 
     get_state(POS_X, POS_Y)
     print_state()
-    return POS_X,POS_Y
+    return POS_X, POS_Y
 
 
 local_dict = {}
+
 
 def intialize_game(the_map):
     global local_dict
